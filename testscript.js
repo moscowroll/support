22a0bf6f3b9e739520d9d05a4d4ca4b491283aec
Pavlo <pahra2012@yandex.ru>
2019-04-30 22:43:35 +0300

    sec w reg html+css without forms

---
+++ testscript.js
@@ -0,0 +1,27 @@
+// var names = ["#checkbox1", "#checkbox2", "#checkbox3"]
+// checkbox_on(function(){
+//   $(x).prop('checked', true);
+// }, 1000);
+//
+//
+// checkbox_off(function(){
+//   $(x).prop('checked', false);
+// }, 1000);
+//
+//
+$(document).ready(function() {
+  // for (var i = 0; i < 4; i++) {
+  //   alert("Javacript подключен");
+  //   alert("i");
+  //
+  // }
+  // console.log("lnfwsikrsr");
+    $("#checkbox1").prop('value', "huy");
+
+}
+
+//
+//
+// console.log("HUY");
+
+ // alert("Javacript подключен");
